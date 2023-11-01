3fb26a12f9515fc4034c2ada14ec94ee3bf1499e
Olena Markevych <markevich.elena8@gmail.com>
2023-11-01 19:17:27 +0200

    add hw3

---
+++ src/ua/markevych/hw3/Task2.java
@@ -0,0 +1,35 @@
+package ua.markevych.hw3;
+
+//Видалити з рядка пробіли і визначити, чи він є перевертнем (паліндромом)
+
+public class Task2 {
+    public static void main(String[] args) {
+        String text = "  It is a bad idea to use a prefix to know the type of variable ";
+        System.out.println(text);
+        String strwithout = text.replace(" ", "");
+        System.out.println(strwithout);
+        palindrom(strwithout);
+
+    }
+
+    public static String reversString(String str) { // метод reversString переобразує введену користувачем строку до дзеркального відображення
+        String str1 = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            str1 += str.charAt(i);
+        }
+        return str1;
+    }
+
+    public static void palindrom(String str) {
+        String s = reversString(str);
+        System.out.println(s);
+
+        if (str.equals(s)) {
+            System.out.println("рядок є паліндромом");
+        } else {
+            System.out.println("рядок не паліндром");
+        }
+
+    }
+
+}
---
+++ src/ua/markevych/hw3/Task3.java
@@ -0,0 +1,16 @@
+package ua.markevych.hw3;
+//Підрахувати кількість слів у рядку
+// Вводиться рядок, що складається зі слів, розділених пробілами.
+// Потрібно порахувати кількість слів у ній.
+
+public class Task3 {
+    public static void main(String[] args) {
+        String text = "It is a bad idea to use a prefix to know the type of variable";
+        System.out.println(text);
+        countWords(text);
+    }
+    public static void countWords(String str){
+        int countWords = str.split("\\s").length;
+        System.out.println("кількість слів  в рядку = " + countWords);
+    }
+}
