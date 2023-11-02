02d19d3b7ea925e3bffd8c53e4b0867757595454
OlenaMarkevych <149398089+olenamarkevych@users.noreply.github.com>
2023-11-02 10:49:30 +0200

    Update Task4.java

--- src/ua/markevych/hw1/Task4.java
+++ src/ua/markevych/hw1/Task4.java
@@ -2,9 +2,10 @@ public class Task4 {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
+        a = b;
+        b = a;
         System.out.print("a = " + a + ", ");
         System.out.println("b = " + b);
-        System.out.print("a = " + b + ", ");
-        System.out.println("b = " + a);
+        
     }
 }
