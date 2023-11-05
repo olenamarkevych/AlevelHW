f071dc25b3852c8fd2b73cacf979d5f2e684ba10
Olena Markevych <markevich.elena8@gmail.com>
2023-11-06 00:33:12 +0200

    add hw3

--- src/ua/markevych/hw3/Task3.java
+++ src/ua/markevych/hw3/Task3.java
@@ -6,11 +6,13 @@ package ua.markevych.hw3;
 
 public class Task3 {
     public static void main(String[] args) {
-        String text = "It is a bad idea to use a prefix to know the type of variable";
+        String text = "     It is a bad idea  to use a prefix to know the type of variable    ";
         System.out.println(text);
         countWords(text);
     }
     public static void countWords(String str){
+        str = str.replaceAll("\\s+", " ");
+        str = str.trim();
         int countWords = str.split("\\s").length;
         System.out.println("кількість слів  в рядку = " + countWords);
     }
