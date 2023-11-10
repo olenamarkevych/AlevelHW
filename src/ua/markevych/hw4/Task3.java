7abd72a88c7ae7929fd9acb6d647182bee301967
Olena Markevych <markevich.elena8@gmail.com>
2023-11-10 09:52:38 +0200

    add hw3

--- src/ua/markevych/hw4/Task3.java
+++ src/ua/markevych/hw4/Task3.java
@@ -8,25 +8,28 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Task3 {
     public static void main(String[] args) {
-        int [] myArray = new int [2000];
+        int[] myArray = new int[2000];
         fillArray(myArray);
         System.out.println();
         System.out.println(myArray.length);
         System.out.println(Arrays.toString(myArray));
-        int[] myArray1 = Arrays.copyOf(myArray, myArray.length);
-        changeNumbers(myArray1);
+        int[] myArray1 = changeNumbers(myArray);
         System.out.println(Arrays.toString(myArray1));
     }
+
     public static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = ThreadLocalRandom.current().nextInt(1,1000);
+            array[i] = ThreadLocalRandom.current().nextInt(1, 1000);
         }
     }
-    public static void changeNumbers (int [] numbers){
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i]%2 == 0){
-                numbers[i] = 0;
+
+    public static int[] changeNumbers(int[] numbers) {
+        int[] numbers1 = Arrays.copyOf(numbers, numbers.length);
+        for (int i = 0; i < numbers1.length; i++) {
+            if (numbers1[i] % 2 == 0) {
+                numbers1[i] = 0;
             }
         }
+        return numbers1;
     }
 }
