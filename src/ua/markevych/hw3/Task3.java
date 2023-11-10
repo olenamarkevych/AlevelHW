7f6c24f56351e3c43881f8df1587d8b1173301bb
Olena Markevych <markevich.elena8@gmail.com>
2023-11-10 09:14:47 +0200

    add hw3

--- src/ua/markevych/hw3/Task3.java
+++ src/ua/markevych/hw3/Task3.java
@@ -3,18 +3,20 @@ package ua.markevych.hw3;
 // Вводиться рядок, що складається зі слів, розділених пробілами.
 // Потрібно порахувати кількість слів у ньому.
 
-    public class Task3 {
-        public static void main(String[] args) {
-            String text = "It is a bad idea to use a prefix to know the type of variable";
-            System.out.println(text);
-            countWords(text);
-        }
-        public static void countWords(String str){
-            str = str.replaceAll("\\s+", " ");
-            str = str.trim();
-            int countWords = str.split("\\s").length;
-            System.out.println("кількість слів  в рядку = " + countWords);
-        }
+public class Task3 {
+    public static void main(String[] args) {
+        String text = "It is a bad idea       to use a prefix to know the type of variable";
+        System.out.println(text);
+        int words = countWords(text);
+        System.out.println("кількість слів  в рядку = " + words);
     }
+
+    public static int countWords(String str) {
+        str = str.replaceAll("\\s+", " ");
+        str = str.trim();
+        int countWords = str.split("\\s").length;
+        return countWords;
+    }
+}
 
 
