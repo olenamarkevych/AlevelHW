112e48299c28ebfa0e392cdb5ea8ae98ba78527a
Olena Markevych <markevich.elena8@gmail.com>
2023-11-10 10:52:49 +0200

    add hw4

--- src/ua/markevych/hw4/Task1.java
+++ src/ua/markevych/hw4/Task1.java
@@ -9,38 +9,44 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Task1 {
     public static void main(String[] args) {
-        int[] myArray = new int [400];
+        int[] myArray = new int[400];
         fillArray(myArray);
         System.out.println();
-       System.out.println(myArray.length);
+        System.out.println(myArray.length);
 
-
-        double seredAr = arithmetic (myArray);
+        double seredAr = arithmetic(myArray);
         System.out.println("середнє аріфметичне = " + seredAr);
-        double serGeom = geometric (myArray);
+        double serGeom = geometric(myArray);
         System.out.println("середнє геометричне = " + serGeom);
     }
-    public static void  fillArray (int[] array){
+
+    public static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt(1, 10);
-                System.out.print(array[i]);
+            System.out.print(array[i] + " ");
         }
     }
-   public static double arithmetic (int [] arith) {
-       double arSum = 0;
-       for (int i = 0; i < arith.length; i++) {
-           arSum = arSum + arith[i];
-       }
-         double resultAr = arSum / arith.length;
-       return resultAr;
 
-   }
-    public static double geometric (int [] geom) {
+    public static double arithmetic(int[] arith) {
+        double arSum = 0;
+        for (int i = 0; i < arith.length; i++) {
+            arSum = arSum + arith[i];
+        }
+        double resultAr = arSum / arith.length;
+        return resultAr;
+
+    }
+
+    public static double geometric(int[] geom) {
         double geomSum = 1;
+        double count = 0;
         for (int i = 0; i < geom.length; i++) {
             geomSum = geomSum * geom[i];
+            count = count + 1;
+
         }
-        double resultGeom = Math.sqrt(geomSum);
+        double pows = 1 / count;
+        double resultGeom = Math.pow(geomSum, pows);
         return resultGeom;
 
     }
