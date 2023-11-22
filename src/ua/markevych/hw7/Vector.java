2fc0737687c0de40c67da05c236d388306ca2095
Olena Markevych <markevich.elena8@gmail.com>
2023-11-22 12:34:32 +0200

    add hw7

--- src/ua/markevych/hw7/Vector.java
+++ src/ua/markevych/hw7/Vector.java
@@ -1,5 +1,7 @@
 package ua.markevych.hw7;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Vector {
@@ -39,20 +41,21 @@ public class Vector {
     }
 
     //метод обчислює суму векторів
-    public void sumVector(double x1, double y1, double z1) {
-        double sum1 = x + x1;
-        double sum2 = y + y1;
-        double sum3 = z + z1;
-        System.out.println("Сума векторів = {" + sum1 + "," + sum2 + "," + sum3 + "}");
+    public double[] sumVector(double x1, double y1, double z1) {
+        double[] sum = new double[3];
+        sum[0] = x + x1;
+        sum[1] = y + y1;
+        sum[2] = z + z1;
+        return sum;
     }
 
-
     //метод обчислює  різницю векторів
-    public void risnVector(double x1, double y1, double z1) {
-        double r1 = x - x1;
-        double r2 = y - y1;
-        double r3 = z - z1;
-        System.out.println("Різниця векторів = {" + r1 + "," + r2 + "," + r3 + "}");
+    public double[] risnVector(double x1, double y1, double z1) {
+        double[] risn = new double[3];
+        risn[0] = x - x1;
+        risn[1] = y - y1;
+        risn[2] = z - z1;
+        return risn;
     }
 
     //метод приймає ціле число N, та повертає масив випадкових
@@ -72,7 +75,7 @@ public class Vector {
 
 
     public static void main(String[] args) {
-        Vector firstlengs = new Vector(1, 1, 1);
+        Vector firstlengs = new Vector(2, 2, 2);
         Vector secondlengs = new Vector(2, 1, 3);
         System.out.println("Довжина вектора1 =  " + (firstlengs.lengthVector(1, 1, 1)));
         System.out.println("Довжина вектора2 =  " + (secondlengs.lengthVector(2, 1, 3)));
@@ -80,9 +83,9 @@ public class Vector {
         System.out.println("Добуток вектора1 = " + scaldob.dobutVector(2, 2, 2));
         System.out.println("Добуток вектора2 = " + firstlengs.dobutVector(2, 2, 2));
         System.out.println("Косінус кута = " + firstlengs.cosVector(2, 3, 1));
-        firstlengs.sumVector(2, 3, 1);
-        firstlengs.risnVector(2, 3, 1);
-        randomN(6);
+        System.out.println("Сума векторів = " + Arrays.toString(firstlengs.sumVector(2, 3, 1)));
+        System.out.println("Різниця векторів = " + Arrays.toString(firstlengs.risnVector(2, 3, 1)));
+        randomN(7);
     }
 }
 
