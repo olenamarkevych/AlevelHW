49eaf37da569653802cc4eae333866330be85d39
Olena Markevych <markevich.elena8@gmail.com>
2023-11-24 17:02:46 +0200

    add hw7

--- src/ua/markevych/hw7/Vector.java
+++ src/ua/markevych/hw7/Vector.java
@@ -1,6 +1,5 @@
 package ua.markevych.hw7;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -16,45 +15,46 @@ public class Vector {
         this.y = y;
         this.z = z;
     }
+    public Vector() {
+
+    }
 
 
     // метод обчислює довжину вектора
-    public double lengthVector(double x, double y, double z) {
+    public double lengthVector() {
         double lengV = Math.abs(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)));
         return lengV;
     }
 
     //метод обчислює векторний добуток з іншим вектором
-    public double dobutVector(double x1, double y1, double z1) {
-        double dobut = x * x1 + y * y1 + z * z1;
-        //  System.out.println("Довжина вектора =  " + lengV);
+    public double dobutVector(Vector vector) {
+        double dobut = x * vector.x + y * vector.y + z * vector.z;
         return dobut;
     }
 
     //метод обчислює косинус кута між векторами
-    public double cosVector(int x1, int y1, int z1) {
-        double scal = x * x1 + y * y1 + z * z1;
+    public double cosVector(Vector vector) {
+        double scal = x * vector.x + y * vector.y + z * vector.z;
         double v1 = Math.abs(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)));
-        double v2 = Math.abs(Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(z1, 2)));
+        double v2 = Math.abs(Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2) + Math.pow(vector.z, 2)));
         double cosin = scal / (v1 * v2);
         return cosin;
     }
-
     //метод обчислює суму векторів
-    public double[] sumVector(double x1, double y1, double z1) {
-        double[] sum = new double[3];
-        sum[0] = x + x1;
-        sum[1] = y + y1;
-        sum[2] = z + z1;
+    public Vector sumvector(Vector vector) {
+        Vector sum = new Vector();
+        sum.x = x + vector.x;
+        sum.y = y + vector.y;
+        sum.z = z + vector.z;
         return sum;
     }
 
     //метод обчислює  різницю векторів
-    public double[] risnVector(double x1, double y1, double z1) {
-        double[] risn = new double[3];
-        risn[0] = x - x1;
-        risn[1] = y - y1;
-        risn[2] = z - z1;
+    public Vector risnVector(Vector vector) {
+        Vector risn = new Vector();
+        risn.x = x - vector.x;
+        risn.y = y - vector.y;
+        risn.z = z - vector.z;
         return risn;
     }
 
@@ -73,18 +73,22 @@ public class Vector {
         }
     }
 
+    public void display() {
+        System.out.println("Vector: (" + x + ", " + y + ", " + z + ")");
+    }
 
     public static void main(String[] args) {
         Vector firstlengs = new Vector(2, 2, 2);
         Vector secondlengs = new Vector(2, 1, 3);
-        System.out.println("Довжина вектора1 =  " + (firstlengs.lengthVector(1, 1, 1)));
-        System.out.println("Довжина вектора2 =  " + (secondlengs.lengthVector(2, 1, 3)));
-        Vector scaldob = new Vector(1, 1, 9);
-        System.out.println("Добуток вектора1 = " + scaldob.dobutVector(2, 2, 2));
-        System.out.println("Добуток вектора2 = " + firstlengs.dobutVector(2, 2, 2));
-        System.out.println("Косінус кута = " + firstlengs.cosVector(2, 3, 1));
-        System.out.println("Сума векторів = " + Arrays.toString(firstlengs.sumVector(2, 3, 1)));
-        System.out.println("Різниця векторів = " + Arrays.toString(firstlengs.risnVector(2, 3, 1)));
+        System.out.println("Довжина вектора1 =  " + (firstlengs.lengthVector()));
+        System.out.println("Довжина вектора2 =  " + (secondlengs.lengthVector()));
+        System.out.println("Добуток ветора1 на вектор2 = " + firstlengs.dobutVector(secondlengs));
+        System.out.println("Косінус кута = " + firstlengs.cosVector(secondlengs));
+        Vector sum = firstlengs.sumvector(secondlengs);
+        System.out.print("Сума векторів = ");
+        sum.display();
+        System.out.print("Різниця векторів = ");
+        firstlengs.risnVector(secondlengs).display();
         randomN(8);
     }
 }
