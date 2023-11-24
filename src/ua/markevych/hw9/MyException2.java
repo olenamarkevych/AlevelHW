b9cb237ce636c724a9a2e55d2b04ac17651c299a
Olena Markevych <markevich.elena8@gmail.com>
2023-11-24 15:32:29 +0200

    add hw9

--- src/ua/markevych/hw9/MyException2.java
+++ src/ua/markevych/hw9/MyException2.java
@@ -4,6 +4,10 @@ public class MyException2 extends Exception {
     MyException2(double number) {
         super(String.valueOf(number));
     }
+
+    public MyException2() {
+
+    }
 }
 
 class MySqrt {
@@ -15,20 +19,23 @@ class MySqrt {
 
         }
         System.out.println(number);
-         num = Math.sqrt(number);
+        num = Math.sqrt(number);
         return num;
 
     }
 
     public static void f(double number1) throws MyException2 {
-        double num = g(number1);
-        throw new MyException2(num);
+        try {
+            double num = g(number1);
+        } catch (MyException2 g) {
+            throw new MyException2();
+        }
 
     }
 
     public static void main(String[] args) throws MyException2 {
         try {
-            f(8);
+            f(7);
         } catch (MyException2 f) {
             System.out.println(f.getMessage());
         }
