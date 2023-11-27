5ba8cb4e7d083c1906d1745d7cabd13d4c96d3c7
Olena Markevych <markevich.elena8@gmail.com>
2023-11-27 22:27:20 +0200

    add hw10

--- src/ua/markevych/hw10/PhoneBook.java
+++ src/ua/markevych/hw10/PhoneBook.java
@@ -2,6 +2,7 @@ package ua.markevych.hw10;
 //Реалізувати метод пошуку індексу телефону в масиві, якщо телефону немає – повернути порожній
 //Optional.
 
+import java.util.Objects;
 import java.util.Optional;
 
 class PhoneBook {
@@ -11,17 +12,22 @@ class PhoneBook {
         PHONE_BOOK[0] = "016/161616";
         PHONE_BOOK[1] = "016/161617";
         PHONE_BOOK[2] = "016/161618";
-        System.out.println(findIndexByPhoneNumber("016/161616"));
+        Optional<Integer> index = findIndexByPhoneNumber("016/1616162");
+        if (index.isPresent()) {
+            System.out.println(index.get());
+        } else {
+            System.out.println("not find");
+        }
         System.out.println(findIndexByPhoneNumber("1"));
     }
 
     public static Optional<Integer> findIndexByPhoneNumber(String phoneNumber) {
         for (int i = 0; i < PHONE_BOOK.length; i++) {
-            if (PHONE_BOOK[i] == phoneNumber) {
+            if (Objects.equals(PHONE_BOOK[i], phoneNumber)) {
                 return Optional.of(i);
             }
         }
-        return null;
+        return Optional.empty();
     }
 }
 
